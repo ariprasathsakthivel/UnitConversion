1a106de3cf03fcbe52d1086eaa0415440376440e
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-18 14:02:38 +0530

    [Ariprasath] Add: Testcode for conversion of feet to yard. Actual code for feet to yard conversion

--- TestCode.py
+++ TestCode.py
@@ -2,7 +2,7 @@
 @Author: Ariprasath
 @Date: 2021-09-17 17:09:50
 @Last Modified by: Ariprasath
-@Last Modified time: 2021-09-17
+@Last Modified time: 2021-09-18
 Title: Test the unit conversion module using pytest
 '''
 
@@ -44,3 +44,32 @@ class TestUnitCOnversion():
 
         #Test case-8
         assert self.obj1.feet_inch(1)<13
+
+
+        
+    def test_feet_to_yard(self,obj):
+
+        #Equality checks
+        #Test case-1  Zero check
+        assert self.obj1.feet_yard(0)==0
+
+        #Test case-2 Null check
+        assert self.obj1.feet_yard(None)==None
+
+        #Test case-3  Reference check
+        assert self.obj1.feet_yard(1)==0.33
+        #Test case-4  Type check
+        assert type(self.obj1.feet_yard(1))==float
+
+        #Test case-5  Value check
+        assert self.obj1.feet_yard(6)==2
+
+        #Comparison check
+        #Test case-6 
+        assert self.obj1.feet_yard(1)!=1
+
+        #Test case-7
+        assert self.obj1.feet_yard(1)<1
+
+        #Test case-8
+        assert self.obj1.feet_yard(1)>0
--- UnitConversion.py
+++ UnitConversion.py
@@ -2,8 +2,8 @@
 @Author: Ariprasath
 @Date: 2021-09-17 17:11:50
 @Last Modified by: Ariprasath
-@Last Modified time: 2021-09-17
-Title: convert the lengths from feet to inch
+@Last Modified time: 2021-09-18
+Title: convert the lengths from feet to inch and feet to yard
 '''
 
 
@@ -27,6 +27,26 @@ class UnitConv():
             elif length >=0:
                 length_inch=length*12
                 return length_inch
+            else:
+                raise Exception
+        except Exception as e:
+            print(e)
+
+    def feet_yard(self,length):
+        '''
+        Description:
+            Converts the length from feet to yard
+        Parameter:
+            length(int): length in feet
+        Return:
+            length_yard(float): length in inch with 2 decimal precision
+        '''
+        try:
+            if length==None:
+                return None
+            elif length >=0:
+                length_yard=length/3
+                return round(length_yard,2)
             else:
                 raise Exception
         except Exception as e:
